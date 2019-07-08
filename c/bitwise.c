b727946ed2245a638cab207dc257cf909d86fedf
Chloe Kudryavtsev <toast@toastin.space>
2019-07-08 00:56:47 -0400

    [c] bitwise left-shift as *2
    In C, integers are not "BigNum"s.
    As such, they have a specific amount of bits allocated to them.
    Normally, the compiler will catch possible shifting overflows applied to
    signed integers and try to prevent them, setting the value to (e.g) 0.
    However, when both parts of the expression are variables, every compiler
    I checked didn't actually do that (not even a warning!).
    
    Therefore, if someone forgets to do bounds checking/testing (likely for
    people seeking to prematurely optimize their programs), they won't
    realize their "wrapper" is actually giving faulty data depending on
    inputs.
    
    Play around with the value of i and n and try inserting non-variables
    into their respective fields.
    It can be quite difficult to predict exactly how a compiler will handle
    a given scenario!
    
    P.S. Most compilers already perform the "optimization" in question, but
    *with* bounds checking.

--- c/bitwise.c
+++ c/bitwise.c
@@ -15,4 +15,12 @@ int main(void) {
     NUM((-10|-10) < 0);
     NUM((10|-10)  < 0);
     NUM((-10|10)  < 0);
+
+    // Bitshifting left is the same thing as multiplying a number by 2.
+    // After writing this proof of concept, I've decided.
+    // I will write a mul() wrapper that detects mulitples of two to optimize.
+    int i = 32, n = 4;
+    NUM(i);
+    NUM(n);
+    NUM(i << n);
 }
